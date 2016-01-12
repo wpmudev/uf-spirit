533a7ed98e41767cdbba77169bc5d3a0715fb4ff
leighsapir <shia@incsub.com>
2016-01-12 00:05:18 -0500

    Home Page
    - Update content
    - Update button presets
    - Update menu presets

---
+++ global-regions/header.php
@@ -0,0 +1,464 @@
+<?php
+/* START_REGION_OUTPUT */
+$region_49bf4f = upfront_create_region(
+			array (
+  'name' => 'header',
+  'title' => 'Header',
+  'type' => 'wide',
+  'scope' => 'global',
+  'container' => 'header',
+  'position' => 10,
+  'allow_sidebar' => true,
+),
+			array (
+  'row' => 5,
+  'breakpoint' => 
+  (array)(array(
+     'tablet' => 
+    (array)(array(
+       'edited' => false,
+       'col' => 24,
+    )),
+     'mobile' => 
+    (array)(array(
+       'edited' => false,
+       'col' => 24,
+    )),
+  )),
+  'background_type' => 'color',
+  'use_padding' => 0,
+  'sub_regions' => 
+  array (
+    0 => false,
+  ),
+  'bg_padding_type' => 'varied',
+  'top_bg_padding_slider' => 0,
+  'top_bg_padding_num' => 0,
+  'bottom_bg_padding_slider' => 0,
+  'bottom_bg_padding_num' => 0,
+  'bg_padding_slider' => 0,
+  'bg_padding_num' => 0,
+  'background_color' => '#ufc0',
+)
+			);
+
+$region_49bf4f->add_element("Uimage", array (
+  'columns' => '4',
+  'margin_left' => '0',
+  'margin_top' => '0',
+  'class' => 'upfront-image_module',
+  'id' => 'module-1452574610984-1587',
+  'options' => 
+  array (
+    'src' => '{{upfront:style_url}}/images/global-regions/header/logo-nav-75x35-8323.png',
+    'srcFull' => '{{upfront:style_url}}/images/global-regions/header/logo-nav.png',
+    'srcOriginal' => '{{upfront:style_url}}/images/global-regions/header/logo-nav.png',
+    'image_title' => '',
+    'alternative_text' => '',
+    'include_image_caption' => false,
+    'image_caption' => '<p>My awesome image caption</p>',
+    'caption_position' => false,
+    'caption_alignment' => false,
+    'caption_trigger' => 'always_show',
+    'image_status' => 'ok',
+    'size' => 
+    (array)(array(
+       'width' => 75,
+       'height' => 35,
+    )),
+    'fullSize' => 
+    (array)(array(
+       'width' => 75,
+       'height' => 35,
+       'top' => 885,
+       'left' => 445,
+    )),
+    'position' => 
+    (array)(array(
+       'top' => 0,
+       'left' => 0,
+    )),
+    'marginTop' => 0,
+    'element_size' => 
+    (array)(array(
+       'width' => 150,
+       'height' => 40,
+    )),
+    'rotation' => 0,
+    'color' => '#ffffff',
+    'background' => '#000000',
+    'captionBackground' => '0',
+    'image_id' => '13',
+    'align' => 'left',
+    'stretch' => false,
+    'vstretch' => true,
+    'quick_swap' => false,
+    'gifImage' => 0,
+    'placeholder_class' => '',
+    'preset' => 'default',
+    'display_caption' => 'showCaption',
+    'type' => 'UimageModel',
+    'view_class' => 'UimageView',
+    'has_settings' => 1,
+    'class' => 'upfront-image',
+    'id_slug' => 'image',
+    'when_clicked' => false,
+    'image_link' => '',
+    'link' => 
+    (array)(array(
+       'type' => 'homepage',
+       'url' => '{{upfront:home_url}}',
+       'target' => false,
+    )),
+    'element_id' => 'image-1452574610977-1691',
+    'top_padding_num' => '15',
+    'bottom_padding_num' => '10',
+    'row' => 13,
+    'lock_padding' => '',
+    'bottom_padding_use' => 'yes',
+    'bottom_padding_slider' => '10',
+    'top_padding_use' => 'yes',
+    'top_padding_slider' => '15',
+  ),
+  'row' => 13,
+  'wrapper_id' => 'wrapper-1452574614121-1081',
+  'new_line' => true,
+  'wrapper_breakpoint' => 
+  array (
+    'tablet' => 
+    array (
+      'edited' => false,
+      'col' => 12,
+      'order' => 0,
+      'clear' => true,
+    ),
+    'mobile' => 
+    array (
+      'edited' => false,
+      'col' => 7,
+      'order' => 0,
+      'clear' => true,
+    ),
+  ),
+  'breakpoint' => 
+  array (
+    'tablet' => 
+    array (
+      'edited' => false,
+      'left' => 0,
+      'col' => 12,
+      'order' => 0,
+    ),
+    'mobile' => 
+    array (
+      'edited' => false,
+      'left' => 0,
+      'col' => 7,
+      'order' => 0,
+    ),
+  ),
+));
+
+$region_49bf4f->add_element("Unewnavigation", array (
+  'columns' => '14',
+  'margin_left' => '0',
+  'margin_top' => '0',
+  'class' => 'upfront-newnavigation_module',
+  'id' => 'module-1452574180065-1300',
+  'options' => 
+  array (
+    'type' => 'UnewnavigationModel',
+    'view_class' => 'UnewnavigationView',
+    'class' => 'c24 upfront-navigation',
+    'has_settings' => 1,
+    'id_slug' => 'unewnavigation',
+    'menu_items' => 
+    array (
+      0 => 
+      (array)(array(
+         'menu-item-db-id' => 7,
+         'menu-item-parent-id' => '0',
+         'menu-item-type' => 'custom',
+         'menu-item-title' => 'SERVICES',
+         'menu-item-url' => '#',
+         'menu-item-object' => 'custom',
+         'menu-item-object-id' => '7',
+         'menu-item-target' => '',
+         'menu-item-position' => 1,
+         'link' => 
+        (array)(array(
+           'type' => 'unlink',
+           'url' => '#',
+           'target' => '',
+        )),
+      )),
+      1 => 
+      (array)(array(
+         'menu-item-db-id' => 8,
+         'menu-item-parent-id' => '0',
+         'menu-item-type' => 'custom',
+         'menu-item-title' => 'ABOUT',
+         'menu-item-url' => '#',
+         'menu-item-object' => 'custom',
+         'menu-item-object-id' => '8',
+         'menu-item-target' => '',
+         'menu-item-position' => 2,
+         'link' => 
+        (array)(array(
+           'type' => 'unlink',
+           'url' => '#',
+           'target' => '',
+        )),
+      )),
+      2 => 
+      (array)(array(
+         'menu-item-db-id' => 9,
+         'menu-item-parent-id' => '0',
+         'menu-item-type' => 'custom',
+         'menu-item-title' => 'TESTIMONIALS',
+         'menu-item-url' => '#',
+         'menu-item-object' => 'custom',
+         'menu-item-object-id' => '9',
+         'menu-item-target' => '',
+         'menu-item-position' => 3,
+         'link' => 
+        (array)(array(
+           'type' => 'unlink',
+           'url' => '#',
+           'target' => '',
+        )),
+      )),
+      3 => 
+      (array)(array(
+         'menu-item-db-id' => 10,
+         'menu-item-parent-id' => '0',
+         'menu-item-type' => 'custom',
+         'menu-item-title' => 'GALLERY',
+         'menu-item-url' => '#',
+         'menu-item-object' => 'custom',
+         'menu-item-object-id' => '10',
+         'menu-item-target' => '',
+         'menu-item-position' => 4,
+         'link' => 
+        (array)(array(
+           'type' => 'unlink',
+           'url' => '#',
+           'target' => '',
+        )),
+      )),
+      4 => 
+      (array)(array(
+         'menu-item-db-id' => 11,
+         'menu-item-parent-id' => '0',
+         'menu-item-type' => 'custom',
+         'menu-item-title' => 'BLOG',
+         'menu-item-url' => '#',
+         'menu-item-object' => 'custom',
+         'menu-item-object-id' => '11',
+         'menu-item-target' => '',
+         'menu-item-position' => 5,
+         'link' => 
+        (array)(array(
+           'type' => 'unlink',
+           'url' => '#',
+           'target' => '',
+        )),
+      )),
+      5 => 
+      (array)(array(
+         'menu-item-db-id' => 12,
+         'menu-item-parent-id' => '0',
+         'menu-item-type' => 'custom',
+         'menu-item-title' => 'CONTACT US',
+         'menu-item-url' => '#',
+         'menu-item-object' => 'custom',
+         'menu-item-object-id' => '12',
+         'menu-item-target' => '',
+         'menu-item-position' => 6,
+         'link' => 
+        (array)(array(
+           'type' => 'unlink',
+           'url' => '#',
+           'target' => '',
+        )),
+      )),
+    ),
+    'preset' => 'default',
+    'allow_sub_nav' => 
+    array (
+      0 => 'no',
+    ),
+    'allow_new_pages' => 
+    array (
+      0 => 'no',
+    ),
+    'element_id' => 'unewnavigation-object-1452574180059-1299',
+    'top_padding_num' => '0',
+    'bottom_padding_num' => '0',
+    'menu_slug' => 'main-menu',
+    'menu_id' => false,
+    'theme_style' => '',
+    'use_padding' => 'yes',
+    'lock_padding' => '',
+    'padding_slider' => '15',
+    'padding_number' => '15',
+    'left_padding_num' => '15',
+    'right_padding_num' => '15',
+    'anchor' => '',
+    'top_padding_use' => 'yes',
+    'top_padding_slider' => '0',
+    'bottom_padding_use' => 'yes',
+    'bottom_padding_slider' => '0',
+    'row' => 4,
+  ),
+  'row' => 4,
+  'wrapper_id' => 'wrapper-1452574228749-1830',
+  'wrapper_breakpoint' => 
+  array (
+    'tablet' => 
+    array (
+      'edited' => false,
+      'col' => 12,
+      'order' => 1,
+      'clear' => true,
+    ),
+    'mobile' => 
+    array (
+      'edited' => false,
+      'col' => 7,
+      'order' => 1,
+      'clear' => true,
+    ),
+  ),
+  'breakpoint' => 
+  array (
+    'tablet' => 
+    array (
+      'edited' => false,
+      'left' => 0,
+      'col' => 12,
+      'order' => 0,
+    ),
+    'mobile' => 
+    array (
+      'edited' => false,
+      'left' => 0,
+      'col' => 7,
+      'order' => 0,
+    ),
+  ),
+));
+
+$region_49bf4f->add_element("Code", array (
+  'columns' => '6',
+  'margin_left' => '0',
+  'margin_top' => '0',
+  'class' => 'upfront-code_element-module',
+  'id' => 'module-1452574180089-1476',
+  'options' => 
+  array (
+    'type' => 'CodeModel',
+    'view_class' => 'CodeView',
+    'class' => 'c24 upfront-code_element-object',
+    'has_settings' => 0,
+    'id_slug' => 'upfront-code_element',
+    'fallbacks' => 
+    (array)(array(
+       'markup' => '<b>Enter your markup here...</b>',
+       'style' => '/* Your styles here */',
+       'script' => '/* Your code here */',
+    )),
+    'element_id' => 'upfront-code_element-object-1452574180088-1304',
+    'top_padding_num' => '15',
+    'bottom_padding_num' => '15',
+    'code_selection_type' => 'Create',
+    'markup' => '<ul class="socials">
+  <li><a class="social-icon youtube" href="http://youtube.com" target="_blank"></a></li>
+  <li><a class="social-icon facebook" href="http://facebook.com" target="_blank"></a></li>
+  <li><a class="social-icon twitter" href="http://twitter.com" target="_blank"></a></li>
+</ul>',
+    'style' => '/* Your styles here */
+.socials {
+    height: 34px;
+    list-style: none;
+    padding: 0px;
+    margin: 0;
+}
+.socials li {
+    display: inline-block;
+}
+.socials li a {
+    display: block;
+    width: 35px;
+    height: 34px;
+    margin: 0 5px;
+}
+
+.social-icon:before {
+    content: "";
+    background: url("{{upfront:style_url}}/ui/sprite.png");
+    background-image: url("{{upfront:style_url}}/ui/sprite.svg"), none;
+    width: 35px;
+    height: 35px;
+    display: block;
+}
+
+.youtube:before {
+ background-position: -63px -222px;
+}
+ .youtube:hover:before,  .youtube.dark { background-position: -223px -222px;
+}
+ .facebook:before {
+ background-position: -63px -383px;
+}
+ .facebook:hover:before,  .facebook.dark { background-position: -223px -383px;
+}
+ .twitter:before {
+ background-position: -63px -543px;
+}
+ .twitter:hover:before,  .twitter.dark { background-position: -223px -543px;
+}',
+    'script' => '/* Your code here */',
+    'row' => 8,
+  ),
+  'row' => 8,
+  'wrapper_id' => 'wrapper-1452574716523-1923',
+  'wrapper_breakpoint' => 
+  array (
+    'tablet' => 
+    array (
+      'edited' => false,
+      'col' => 12,
+      'order' => 2,
+      'clear' => true,
+    ),
+    'mobile' => 
+    array (
+      'edited' => false,
+      'col' => 7,
+      'order' => 2,
+      'clear' => true,
+    ),
+  ),
+  'breakpoint' => 
+  array (
+    'tablet' => 
+    array (
+      'edited' => false,
+      'left' => 0,
+      'col' => 12,
+      'order' => 0,
+    ),
+    'mobile' => 
+    array (
+      'edited' => false,
+      'left' => 0,
+      'col' => 7,
+      'order' => 0,
+    ),
+  ),
+));
+
+$regions->add($region_49bf4f);
+
+/* END_REGION_OUTPUT */
--- layouts/archive-home.php
+++ layouts/archive-home.php
@@ -12,9 +12,9 @@ $home_header = upfront_create_region(
   'allow_sidebar' => true,
 ),
 			array (
-  'row' => 80,
-  'background_type' => 'color',
-  'background_color' => '#c5d0db',
+  'row' => 174,
+  'background_type' => 'image',
+  'background_color' => '#ufc6',
   'version' => '1.0.0',
   'breakpoint' => 
   (array)(array(
@@ -41,8 +41,257 @@ $home_header = upfront_create_region(
   'bottom_bg_padding_num' => 0,
   'bg_padding_slider' => 0,
   'bg_padding_num' => 0,
+  'background_style' => 'full',
+  'background_position_y' => '50',
+  'background_position_x' => '50',
+  'background_image' => '{{upfront:style_url}}/images/archive-home/Homepage-Banner.jpg',
+  'background_image_ratio' => 0.560000000000000053290705182007513940334320068359375,
 )
 			);
 
+$home_header->add_element("Uimage", array (
+  'columns' => '8',
+  'margin_left' => '0',
+  'margin_right' => '0',
+  'margin_top' => '0',
+  'margin_bottom' => '0',
+  'class' => 'module-1452572518182-1660',
+  'id' => 'module-1452572518182-1660',
+  'options' => 
+  array (
+    'src' => '{{upfront:style_url}}/images/archive-home/logo-330x225-1296.png',
+    'srcFull' => '{{upfront:style_url}}/images/archive-home/logo.png',
+    'srcOriginal' => '{{upfront:style_url}}/images/archive-home/logo.png',
+    'image_title' => '',
+    'alternative_text' => '',
+    'include_image_caption' => true,
+    'image_caption' => '<p>My awesome image caption</p>',
+    'caption_position' => false,
+    'caption_alignment' => false,
+    'caption_trigger' => 'always_show',
+    'image_status' => 'ok',
+    'size' => 
+    (array)(array(
+       'width' => 355,
+       'height' => 225,
+    )),
+    'fullSize' => 
+    (array)(array(
+       'width' => 355,
+       'height' => 225,
+    )),
+    'position' => 
+    (array)(array(
+       'top' => 0,
+       'left' => 0,
+    )),
+    'marginTop' => 0,
+    'element_size' => 
+    (array)(array(
+       'width' => 330,
+       'height' => 225,
+    )),
+    'rotation' => 0,
+    'color' => '#ffffff',
+    'background' => '#000000',
+    'captionBackground' => '0',
+    'image_id' => '5',
+    'align' => 'center',
+    'stretch' => true,
+    'vstretch' => true,
+    'quick_swap' => false,
+    'gifImage' => 0,
+    'placeholder_class' => '',
+    'preset' => 'default',
+    'display_caption' => 'showCaption',
+    'type' => 'UimageModel',
+    'view_class' => 'UimageView',
+    'has_settings' => 1,
+    'class' => 'c24 upfront-image',
+    'id_slug' => 'image',
+    'when_clicked' => false,
+    'image_link' => '',
+    'link' => 
+    (array)(array(
+       'type' => 'homepage',
+       'url' => '{{upfront:home_url}}',
+       'target' => false,
+    )),
+    'element_id' => 'image-1452572518178-1011',
+    'top_padding_num' => '15',
+    'bottom_padding_num' => '15',
+    'row' => 17,
+  ),
+  'row' => 6,
+  'sticky' => false,
+  'default_hide' => 0,
+  'hide' => 0,
+  'toggle_hide' => 1,
+  'wrapper_id' => 'wrapper-1452573352753-1930',
+  'new_line' => true,
+  'wrapper_breakpoint' => 
+  array (
+    'tablet' => 
+    array (
+      'edited' => false,
+      'col' => 12,
+      'order' => 0,
+      'clear' => true,
+    ),
+    'mobile' => 
+    array (
+      'edited' => false,
+      'col' => 7,
+      'order' => 0,
+      'clear' => true,
+    ),
+  ),
+  'breakpoint' => 
+  array (
+    'tablet' => 
+    array (
+      'edited' => false,
+      'left' => 0,
+      'col' => 12,
+      'order' => 0,
+    ),
+    'mobile' => 
+    array (
+      'edited' => false,
+      'left' => 0,
+      'col' => 7,
+      'order' => 0,
+    ),
+  ),
+));
+
+$home_header->add_element("Uspacer", array (
+  'columns' => '16',
+  'margin_left' => '0',
+  'margin_right' => '0',
+  'margin_top' => '0',
+  'margin_bottom' => '0',
+  'class' => 'module-1452573423386-1121 upfront-module-spacer',
+  'id' => 'module-1452573423386-1121',
+  'options' => 
+  array (
+    'type' => 'UspacerModel',
+    'view_class' => 'UspacerView',
+    'class' => 'c24',
+    'has_settings' => 0,
+    'id_slug' => 'uspacer',
+    'element_id' => 'spacer-object-1452573423385-1965',
+  ),
+  'row' => 6,
+  'sticky' => false,
+  'default_hide' => 1,
+  'hide' => 0,
+  'toggle_hide' => 0,
+  'wrapper_id' => 'wrapper-1452573423386-1401',
+  'wrapper_breakpoint' => 
+  array (
+    'tablet' => 
+    array (
+      'col' => 12,
+    ),
+    'mobile' => 
+    array (
+      'col' => 7,
+    ),
+  ),
+));
+
+$home_header->add_element("Button", array (
+  'columns' => '24',
+  'margin_left' => '0',
+  'margin_right' => '0',
+  'margin_top' => '0',
+  'margin_bottom' => '0',
+  'class' => 'module-1452572518275-1268',
+  'id' => 'module-1452572518275-1268',
+  'options' => 
+  array (
+    'content' => 'Click here',
+    'href' => '',
+    'linkTarget' => '',
+    'align' => 'center',
+    'type' => 'ButtonModel',
+    'view_class' => 'ButtonView',
+    'class' => 'c24 upfront-button',
+    'has_settings' => 1,
+    'id_slug' => 'ubutton',
+    'preset' => 'arrow-down',
+    'element_id' => 'button-object-1452572518273-1648',
+    'link' => 
+    (array)(array(
+       'type' => 'unlink',
+       'url' => '',
+       'target' => '',
+    )),
+    'top_padding_num' => '475',
+    'bottom_padding_num' => '15',
+    'theme_style' => '',
+    'use_padding' => 'yes',
+    'lock_padding' => '',
+    'padding_slider' => '15',
+    'padding_number' => '15',
+    'left_padding_num' => '15',
+    'right_padding_num' => '15',
+    'anchor' => '',
+    'breakpoint_presets' => 
+    (array)(array(
+       'desktop' => 
+      (array)(array(
+         'preset' => 'arrow-down',
+      )),
+    )),
+    'top_padding_use' => 'yes',
+    'top_padding_slider' => '200',
+  ),
+  'row' => 6,
+  'sticky' => false,
+  'default_hide' => 0,
+  'hide' => 0,
+  'toggle_hide' => 1,
+  'wrapper_id' => 'wrapper-1452573475808-1085',
+  'new_line' => true,
+  'wrapper_breakpoint' => 
+  array (
+    'tablet' => 
+    array (
+      'edited' => false,
+      'col' => 12,
+      'order' => 1,
+      'clear' => true,
+    ),
+    'mobile' => 
+    array (
+      'edited' => false,
+      'col' => 7,
+      'order' => 1,
+      'clear' => true,
+    ),
+  ),
+  'breakpoint' => 
+  array (
+    'tablet' => 
+    array (
+      'edited' => false,
+      'left' => 0,
+      'col' => 12,
+      'order' => 0,
+    ),
+    'mobile' => 
+    array (
+      'edited' => false,
+      'left' => 0,
+      'col' => 7,
+      'order' => 0,
+    ),
+  ),
+));
+
 $regions->add($home_header);
+
+if (file_exists(get_stylesheet_directory() . DIRECTORY_SEPARATOR . 'global-regions' . DIRECTORY_SEPARATOR . 'header.php')) include(get_stylesheet_directory() . DIRECTORY_SEPARATOR . 'global-regions' . DIRECTORY_SEPARATOR . 'header.php');
 
